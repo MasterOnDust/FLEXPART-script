4b34b05c008997eedae0a642865d6e88976d3467
Ove Haugvaldstad <ovewh@outlook.com>
2020-08-24 21:44:06 +0200

    presist dataset in memory

--- plot_time_series.py
+++ plot_time_series.py
@@ -48,13 +48,20 @@ if __name__ == "__main__":
             s_time = pd.to_datetime(d0.time[0].values).strftime('%Y-%m-%d')
         
         date_slices.append(slice(e_time,s_time))
+
+    dsets = []
+    for path in [path_2micron, path_20micron]:
+        dset = xr.open_dataset(path)
+        dset = dset.srr.make_time_seires()
+        dset.to_netcdf(outpath +'/{}_{}_2019'.format(data_var, loc_name) + '.nc')
+        dsets.append(dset.persist()) 
+
     for date_slice in date_slices:
         fig, (ax,ax1) = plt.subplots(2,1,sharex=True, sharey=True, figsize=(16,5))
         
-        for path, ax_i, color, p_size in zip([path_2micron,path_20micron],(ax,ax1),['saddlebrown', 'sandybrown'], p_sizes):
-            dset = xr.open_dataset(path)
-            dset = dset.sel(time=date_slice)
-            ax_i = dset.srr.plot_time_series(ax=ax_i, label = dset.receptor_name + ' ' + p_size)
+        for dset, ax_i, color, p_size in zip(dsets,(ax,ax1),['saddlebrown', 'sandybrown'], p_sizes):
+            temp_dset = dset.sel(time=date_slice)
+            ax_i = temp_dset.srr.plot_time_series(ax=ax_i, label = dset.receptor_name + ' ' + p_size)
             ax_i.legend()
             ax_i.grid(linestyle='-')
         ax1.axes.xaxis.label.set_visible(False)
