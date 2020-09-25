dc1c96581cd3026d06f8db133725909ae5572869
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-09-25 11:36:34 +0200

    moved jobscript

---
+++ src/jobscript.py
@@ -0,0 +1,213 @@
+from params import sites
+import argparse as ap
+import os
+import pandas as pd
+import shutil
+import sys
+import importlib.util
+
+def write_species(folderName):
+    settings = sim.SPEC.species_Params
+    with open(folderName +'/options/SPECIES/SPECIES_001', 'w') as sp:
+        sp.writelines('&SPECIES_PARAMS\n')
+        for option, setting in settings.items():
+            sp.writelines(option + ' = ' + setting + ',\n')
+        sp.writelines('/')
+
+def write_command(folderName):
+    settings =  sim.COMMAND.command
+    with open(folderName + '/options/COMMAND', 'w') as c:
+        c.writelines('&COMMAND\n')
+        for option, setting in settings.items():
+            c.writelines(option + ' = ' + setting + ',\n')
+
+        c.writelines('/')
+
+def write_outgrid(folderName):
+    settings = sim.OUTGRID.outgrid
+    with open(folderName + '/options/OUTGRID', 'w') as o:
+        o.writelines('&OUTGRID\n')
+        for option, setting in settings.items():
+            o.writelines(option + ' = ' + setting + ',\n')
+        o.writelines('/')
+def write_pathnames(folderName):
+    with open(folderName + '/pathnames', 'w') as pn:
+        pn.writelines('./options/\n')
+        pn.writelines('./output/\n')
+        pn.writelines('/\n')
+        pn.writelines(sim.path_to_forcing + '\n')
+        pn.writelines(sim.flexpart_input_path + '\n')
+        pn.writelines('============================================')
+
+def write_release_file(sites, specNum, folderName):
+    nspec = len(sites)
+    rel_comment = sim.RELEASES.comment
+    with open(folderName+'/options/RELEASES', 'w') as r:
+        r.writelines('&RELEASES_CTRL\n')
+        r.writelines('NSPEC = {},\n'.format(nspec))
+        r.writelines('SPECNUM_REL = {}*{},\n'.format(nspec, specNum))
+        for site in sites:
+            sim.RELEASES.LON1 = site.lon
+            sim.RELEASES.LON2 = site.lon
+            sim.RELEASES.LAT1 = site.lat
+            sim.RELEASES.LAT2 = site.lat
+            sim.RELEASES.comment = '\"' + site.comment + rel_comment + '\"'
+            write_single_release(r)
+            sim.RELEASES.comment = rel_comment
+        r.writelines('/')
+
+def write_single_release(outfile):
+    outfile.writelines('/\n')
+    settings = sim.RELEASES.release
+
+    outfile.writelines('&RELEASE\n')
+    for option, setting in settings.items():
+        outfile.writelines(option + ' = ' + setting + ',\n')
+
+
+
+def submit_job(dateI, folderName):
+#   Should make jobfile configurable from simulation.py
+    str_date = dateI.strftime("%Y%m%d-%H")
+    job_file = folderName + '/submit_' + dateI.strftime("%Y%m%d_%H") + '.sh'
+    settings = sim.JOBSCRIPT.job_params
+    with open(job_file, 'w') as fh:
+        fh.writelines("#!/bin/bash\n")
+        for option, setting in settings.items():
+            fh.writelines("#SBATCH --{}={}\n".format(option,setting))
+        fh.writelines("#SBATCH --job-name=FLEXPART_{}\n".format(str_date))
+        fh.writelines("#SBATCH --error={}/out_{}.err\n".format(folderName,str_date))
+        fh.writelines("#SBATCH --output={}/out_{}.out\n".format(folderName,str_date))
+        fh.writelines("#SBATCH --mail-type=FAIL\n")
+        fh.writelines('set -o errexit\n')
+        fh.writelines('set -o nounset\n')
+        fh.writelines('module --quiet purge\n')
+
+        fh.writelines('module load ecCodes/2.9.2-intel-2018b\n')
+        fh.writelines('module load netCDF-Fortran/4.4.4-intel-2018b\n')
+        fh.writelines('export PATH={}:$PATH\n'.format(sim.flexpart_src))
+        fh.writelines('cd {}\n'.format(folderName))
+        fh.writelines("time FLEXPART\n")
+
+        fh.writelines('echo \"{} ,       COMPLETED \" >> {}/COMPLETED_RUNS \n'.format(folderName,sim.abs_path))
+        fh.writelines('exit 0' )
+    os.system("sbatch %s" %job_file)
+
+def makefolderStruct(dateI):
+    folderName = sim.abs_path + '/' + dateI.strftime("%Y%m%d_%H")
+    os.mkdir(folderName)
+    os.mkdir(folderName + '/options')
+    os.mkdir(folderName + '/output')
+    os.mkdir(folderName +'/options/SPECIES')
+    #for filename in os.listdir(sim.flexpart_input_path):
+    #    shutil.copy(sim.flexpart_input_path + '/' + filename, folderName + '/options/')
+
+    write_pathnames(folderName)
+
+    return folderName
+
+def tracing_the_winds_sites():
+    site_dict = {'SACOL': sites(lon='104.137', lat='35.964', comment='SACOL '),
+                'BADOE': sites(lon='111.170', lat = '39.003', comment='BADOE '),
+                'YINCHUAN': sites(lon='106.101', lat='38.500', comment='YINCHUAN '),
+                'LINGTAI': sites(lon ='107.789', lat='35.065', comment='LINGTAI '),
+                'LUOCHUAN': sites(lon='109.424', lat = '35.710', comment='LUOCHUAN '),
+                'SHAPOTOU': sites(lon='105.0475', lat='37.749', comment='SHAPOTOU '),
+                'LANTIAN' : sites(lon ='109.256', lat='34.180', comment='LANTIAN ')}
+    return site_dict
+
+def setup_flexpart(site_list):
+    with open(sim.abs_path + "/COMPLETED_RUNS", "w") as cr:
+        cr.writelines("Path,            STATUS \n")
+    s = pd.to_datetime(sim.start_date+' '+sim.start_time)
+    e = pd.to_datetime(sim.end_date+' '+sim.end_time)
+    date_range = pd.date_range(start=s,
+                           end=e, freq=sim.time_step)
+    sim_lenght = pd.to_timedelta(sim.lenght_of_simulation)
+    release_duration = pd.to_timedelta(sim.release_intervall)
+
+    for date in date_range:
+
+        sim.COMMAND.IBDATE = (date+sim_lenght).strftime('%Y%m%d')
+        sim.COMMAND.IBTIME = (date+sim_lenght).strftime('%H%M%S')
+        sim.COMMAND.IEDATE = date.strftime('%Y%m%d')
+        sim.COMMAND.IETIME = date.strftime('%H%M%S')
+
+        sim.RELEASES.IDATE1 = (date+release_duration).strftime('%Y%m%d')
+        sim.RELEASES.ITIME1 = (date+release_duration).strftime('%H%M%S')
+        sim.RELEASES.IDATE2 = date.strftime('%Y%m%d')
+        sim.RELEASES.ITIME2 = date.strftime('%H%M%S')
+        folderName = makefolderStruct(date)
+        write_command(folderName)
+        write_outgrid(folderName)
+        write_species(folderName)
+        write_release_file(site_list, '1', folderName)
+        if test:
+            break
+        elif test_and_submit:
+            submit_job(date, folderName)
+            break
+        else:
+            submit_job(date,folderName)
+
+
+
+def createParentDir():
+    try:
+        os.mkdir(sim.abs_path)
+    except FileExistsError:
+        askConfirmation = input("""This folder already exists,
+        do you want to delete it? (y/n)""")
+        if askConfirmation.strip() == 'y':
+            shutil.rmtree(sim.abs_path)
+            os.mkdir(sim.abs_path)
+        else:
+            sys.exit()
+    os.chdir(sim.abs_path)
+
+
+if __name__=="__main__":
+
+    parser = ap.ArgumentParser()
+    parser.add_argument('--test', help="Setup one simulation, for check if setting is correct without submiting", action="store_true")
+    parser.add_argument('--testAndSubmit', '--ts', help ="Setup one simulation and submit job", action="store_true")
+    parser.add_argument('path', help='Path to file containting simulation settings', default='flexpart_setup.py')
+    parser.add_argument('--absPath', '--ap', help='Absolute path to topdirectory where flexpart simulation will be created', default=None)
+    parser.add_argument('--edate','--ed', help='date of last flexpart run', default=None)
+    parser.add_argument('--bdate', '--bd', help='date of fist flexpart run', default=None)
+    args = parser.parse_args()
+
+    test = False
+    test_and_submit = False
+
+    if args.test:
+        test = True
+    elif args.testAndSubmit:
+        test_and_submit = True
+    path = args.path
+    arg_path = args.absPath
+    e_date = args.edate
+    b_date = args.bdate
+    spec = importlib.util.spec_from_file_location('*', path)
+    sim = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(sim)
+
+    if b_date:
+        sim.start_date = b_date
+    if e_date:
+        sim.end_date = e_date
+
+    if arg_path:
+        sim.abs_path = arg_path
+
+    sim.abs_path = os.path.abspath(sim.abs_path)
+
+    site_dict = tracing_the_winds_sites()
+    if sim.locations == 'ALL':
+        site_list = [site_dict[loc] for loc in site_dict.keys()]
+    else:
+        site_list = [site_dict[loc] for loc in sim.locations]
+    createParentDir()
+    setup_flexpart(site_list)
+
+
---
+++ src/params.py
@@ -0,0 +1,235 @@
+
+class Species_Params:
+    def __init__(self, **kwargs):
+        self.sp = {
+        'PSPECIES' : 'FINE-SILT'       ,
+        'PDECAY'  : '-9.9'    ,
+        'PWETA_GAS': '-9.0000002E-10',
+        'PWETB_GAS' : '-9.900000'    ,
+        'PCRAIN_AERO' :  '1.000000' ,
+        'PCSNOW_AERO' : '1.000000'    ,
+        'PCCN_AERO'   : '0.9000000'    ,
+        'PIN_AERO'   : '0.1000000'    ,
+        'PRELDIFF':  '-9.900000'    ,
+        'PHENRY'  : '-9.0000002E-10',
+        'PF0' : '-9.000000'    ,
+        'PDENSITY' :  '1400.000'    ,
+        'PDQUER' : '2.0E-06',
+        'PDSIGMA' : '1.250'    ,
+        'PDRYVEL' :'-9.990'    ,
+        'PWEIGHTMOLAR' :  '-9.900000'    ,
+        'POHCCONST' : '-9.0000002E-10',
+        'POHDCONST':  '-9.9'   ,
+        'POHNCONST':  '2.0' }
+        for k, o in kwargs.items():
+            self.sp[k] = o
+
+
+    @property
+    def species_Params(self):
+        return self.sp
+
+class Job_params:
+  def __init__(self, **kwargs):
+      self.jb = {'account': 'nn2806k',
+               'time': '1:00:00',
+               'ntasks':'1',
+               'mem-per-cpu':'6G',
+               'mail-user':'ovewh@student.geo.uio.no'}
+      for k, o in kwargs.items():
+          if k == 'mem_per_cpu':
+              self.jb['mem-per-cpu'] = o
+          else:
+              self.jb[k] = o
+  @property
+  def job_params(self):
+      return self.jb
+
+
+
+class Outgrid:
+    def __init__(self,**kwargs):
+        self.og ={
+             'OUTLON0' :'60.00000'    ,
+             'OUTLAT0' :'30.00000'    ,
+             'NUMXGRID' :    '680',
+             'NUMYGRID' :'320',
+             'DXOUT' : '0.1000000'    ,
+             'DYOUT' :  '0.1000000'    ,
+             'OUTHEIGHTS':'100.0000    ,   500.0000    ,   1000.000    ,   5000.000'
+        }
+        for k, o in kwargs.items():
+            self.og[k] = o
+    @property
+    def outgrid(self):
+        return self.og
+
+class Releases:
+    def __init__(self, **kwargs):
+        self.r = {'PARTS':'50000',
+                  'Z1':'100',
+                  'Z2': '100',
+                  'ZKIND': '1',
+                  'MASS' : '1.0000E8'
+                 }
+
+        for k, o in kwargs.items():
+            self.r[k] = o
+    @property
+    def IDATE1(self):
+        return self.r['IDATE1']
+    @IDATE1.setter
+    def IDATE1(self, bdate):
+        self.r['IDATE1'] = bdate
+    @property
+    def ITIME1(self):
+        return self.r['ITIME1']
+    @ITIME1.setter
+    def ITIME1(self, etime):
+        self.r['ITIME1'] = etime
+    @property
+    def IDATE2(self):
+        return self.r['IDATE2']
+    @IDATE2.setter
+    def IDATE2(self, bdate):
+        self.r['IDATE2'] = bdate
+    @property
+    def ITIME2(self):
+        return self.r['ITIME2']
+    @ITIME2.setter
+    def ITIME2(self, btime):
+        self.r['ITIME2'] = btime
+
+    @property
+    def LON1(self):
+        return self.r['LON1']
+    @LON1.setter
+    def LON1(self, lon):
+        self.r['LON1'] = lon
+    @property
+    def LAT1(self):
+        return self.r['LAT1']
+
+    @LAT1.setter
+    def LAT1(self, lat):
+        self.r['LAT1'] = lat
+
+    @property
+    def LON2(self):
+        return self.r['LON2']
+
+    @LON2.setter
+    def LON2(self, lon):
+        self.r['LON2'] = lon
+
+    @property
+    def LAT2(self):
+        return self.r['LAT2']
+
+    @LAT2.setter
+    def LAT2(self, lat):
+        self.r['LAT2'] = lat
+
+    @property
+    def comment(self):
+        return self.r['COMMENT']
+
+    @comment.setter
+    def comment(self, comment):
+        self.r['COMMENT'] = comment
+    @property
+    def release(self):
+        return self.r
+
+
+class Command:
+    def __init__(self, **kwargs):
+        self.c = {
+             'LDIRECT': '1',
+             'IBDATE': '20190309',
+             'IBTIME': '060000',
+             'IEDATE': '20190310',
+             'IETIME': '120000',
+             'LOUTSTEP':       '3600',
+             'LOUTAVER':       '3600',
+             'LOUTSAMPLE':       '900',
+             'ITSPLIT':  '99999999',
+             'LSYNCTIME':        '900',
+             'CTL' : '-5.0000000'    ,
+             'IFINE':          '4',
+             'IOUT':    '1',
+             'IPOUT':          '0',
+             'LSUBGRID' :          '0',
+             'LCONVECTION' :          '0',
+             'LAGESPECTRA':          '0',
+             'IPIN':          '0',
+             'IOUTPUTFOREACHRELEASE' :          '0',
+             'IFLUX':          '0',
+             'MDOMAINFILL':          '0',
+             'IND_SOURCE':          '1',
+             'IND_RECEPTOR':          '1',
+             'MQUASILAG':          '0',
+             'NESTED_OUTPUT':          '0',
+             'LINIT_COND':          '0',
+             'SURF_ONLY':          '0',
+             'CBLFLAG':        '0',
+             'OHFIELDS_PATH':          '../../flexin/',
+        }
+
+        for k, o in kwargs.items():
+            self.c[k] = o
+    @property
+    def command(self):
+        return self.c
+    @property
+    def IBDATE(self):
+        return self.c['IBDATE']
+    @IBDATE.setter
+    def IBDATE(self, bdate):
+        self.c['IBDATE'] = bdate
+
+    @property
+    def IEDATE(self, edate):
+        return self.c['IEDATE']
+    @IEDATE.setter
+    def IEDATE(self, edate):
+        self.c['IEDATE'] = edate
+
+
+    @property
+    def IETIME(self):
+        return self.c['IETIME']
+    @IETIME.setter
+    def IETIME(self, etime):
+        self.c['IETIME'] = etime
+
+
+    @property
+    def IBTIME(self):
+        return self.c['IBTIME']
+    @IBTIME.setter
+    def IBTIME(self, btime):
+        self.c['IBTIME'] = btime
+
+
+class sites:
+    def __init__(self, lon, lat, comment):
+        self.lon = lon
+        self.lat = lat
+        self.comment = comment
+
+
+    @property
+    def LON(self):
+        return self.lon
+
+    @property
+    def LAT(self):
+        return self.lat
+
+    @property
+    def COMMENT(self):
+        return self.comment
+
+
+
