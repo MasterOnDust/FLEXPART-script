10b54c38e6a50eb537c89a6e69415d932cbdc75a
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-10-21 08:51:31 +0200

    added feature to create batches of jobs and make job script 	deleted:    FLEXPART_settings/production_runs/SPRING_CONC_RUNS/conc_20_spring2018.py 	deleted:    FLEXPART_settings/production_runs/SPRING_CONC_RUNS/conc_2_spring2018.py 	deleted:    FLEXPART_settings/testruns/wet_dep_sensitivity_sim/wet_dep_no_rain_scav.py 	deleted:    src/Improved_FLEXPART_runner.ipynb 	modified:   src/jobscript.py 	modified:   src/params.json 	deleted:    src/params.py

--- src/jobscript.py
+++ src/jobscript.py
@@ -36,19 +36,19 @@ def write_pathnames(folderName, paths):
 
 
 
-def write_sbatct_file(jobscript_params,daterange, paths):
-    date0 = daterange.tolist()[0].strftime("%Y%m%d")
-    date1 = daterange.tolist()[-1].strftime("%Y%m%d")
+def write_sbatct_file(jobscript_params,date, paths, batch_number):
+    date0 = date.strftime("%Y%m%d")
+#     date1 = daterange.tolist()[-1].strftime("%Y%m%d")
     job_file = paths['abs_path'] + '/submit_flexpart.sh'
     with open(job_file, 'w') as fh:
         fh.writelines("#!/bin/bash\n")
         for option, setting in jobscript_params.items():
             fh.writelines("#SBATCH --{}={}\n".format(option,setting))
         fh.writelines("#SBATCH --job-name=FLEXPART_{}\n".format(date0))
-        fh.writelines("#SBATCH --error={}/out_{}.err\n".format(paths['abs_path'],date0))
-        fh.writelines("#SBATCH --output={}/out_{}.out\n".format(paths['abs_path'],date0))
+        fh.writelines("#SBATCH --error={}/log/out_%A_%a{}.err\n".format(paths['abs_path'],date0))
+        fh.writelines("#SBATCH --output={}/log/out_%A_%a_{}.out\n".format(paths['abs_path'],date0))
         fh.writelines("#SBATCH --mail-type=FAIL\n")
-        fh.writelines("#SBATCH --array=1-4\n")
+#         fh.writelines("#SBATCH --array=1-{}\n".)
         fh.writelines('set -o errexit\n')
         fh.writelines('set -o nounset\n')
         fh.writelines('module --quiet purge\n')
@@ -56,12 +56,12 @@ def write_sbatct_file(jobscript_params,daterange, paths):
         fh.writelines('module load ecCodes/2.9.2-intel-2018b\n')
         fh.writelines('module load netCDF-Fortran/4.4.4-intel-2018b\n')
         fh.writelines('export PATH={}:$PATH\n'.format(paths["flexpart_src"]))
-        fh.writelines('$(sed -n \"${SLURM_ARRAY_TASK_ID}p\" paths.txt)\n')
+        fh.writelines('DIR=$(sed -n \"${SLURM_ARRAY_TASK_ID}\"' + ' paths{}.txt)\n'.format(batch_number))
         fh.writelines('cd $DIR\n')
         fh.writelines("time FLEXPART\n")
 
-        # fh.writelines('echo \"{} ,       COMPLETED \" >> {}/COMPLETED_RUNS \n'.format(folderName,
-        #             paths["abs_path"]))
+        fh.writelines('echo \"$DIR ,       COMPLETED \" >> {}/COMPLETED_RUNS \n'.format(
+                    paths["abs_path"]))
         fh.writelines('exit 0' )
 
 
@@ -83,7 +83,7 @@ def makefolderStruct(dateI,paths):
             paths dictionary containing FLEXPART paths.
 
         return:
-            path to FLEXPART folder 
+            path to FLEXPART folder
     """
     folderName = paths["abs_path"] + '/' + dateI.strftime("%Y%m%d_%H")
     os.mkdir(folderName)
@@ -101,7 +101,7 @@ def update_dict(d, u):
             d[k] = v
     return d
 
-def write_release_file(path, site_dict, release_dict 
+def write_release_file(path, site_dict, release_dict
                             ,dateI , release_duration):
     release_dict = release_dict.copy()
 
@@ -112,7 +112,7 @@ def write_release_file(path, site_dict, release_dict
         outfile.writelines('/\n')
         rel_comment = release_dict.pop('COMMENT')
         for key, site in site_dict.items():
-            
+
             temp_dict = release_dict
             temp_dict['LON1'] = site['lon']
             temp_dict['LON2'] = site['lon']
@@ -149,11 +149,11 @@ def setup_flexpart(settings=None):
     simulation_params = params['Simulation_params']
     command = params['Command_Params']
     outgrid = params['Outgrid_params']
-    species_params = params['Species_Params'] 
+    species_params = params['Species_Params']
     site_dict = params['Receptor_locations']
     release_dict = params['Release_params']
-    job_pramas = params["Job_params"]
-    
+    job_params = params["Job_params"]
+
     species_params["PSPECIES"] = '\"' +species_params["PSPECIES"]+ '\"'
 
     with open(paths["abs_path"] + "/COMPLETED_RUNS", "w") as cr:
@@ -176,17 +176,28 @@ def setup_flexpart(settings=None):
         write_to_file(outgrid, folderName + '/options/OUTGRID', 'OUTGRID')
         write_to_file(species_params, folderName + '/options/SPECIES/SPECIES_001','SPECIES_PARAMS')
         write_pathnames(folderName,paths)
-        write_release_file(folderName + '/options/RELEASES', site_dict, 
+        write_release_file(folderName + '/options/RELEASES', site_dict,
                                 release_dict,date, release_duration)
         dir_list.append(folderName)
+    batch_size = int(job_params['array'].split('-')[1])
+    batches = [dir_list]
+    if len(dir_list) >= batch_size:
+        batches = [dir_list[x:x+100] for x in range(0, len(dir_list), batch_size)]
 
-    with open(os.path.join(paths['abs_path'], 'paths.txt'), 'w') as outfile:
-         for path in dir_list:
-             outfile.writelines(path + '\n')
-    write_sbatct_file(job_pramas, date_range, paths)
+
+
+    for i, batch in enumerate(batches):
+
+        path_file = os.path.join(paths['abs_path'], f'paths{i}.txt')
+        write_path_file(batch,path_file)
+        job_params['array'] = '1-{}'.format(len(batch)+1)
+        write_sbatct_file(job_params,pd.to_datetime(batch[0].split('/')[-1], format="%Y%m%d_%H"), paths,i)
     return params
 
-
+def write_path_file(batch, file_path):
+    with open(file_path, 'w') as outfile:
+         for path in batch:
+             outfile.writelines(path + '\n')
 
 def createParentDir(paths):
     """create parent directory"""
@@ -201,14 +212,15 @@ def createParentDir(paths):
         else:
             sys.exit()
     os.chdir(paths["abs_path"])
+    os.mkdir('log')
 
 
 if __name__=="__main__":
 
     parser = ap.ArgumentParser()
-    parser.add_argument('--test', help="Setup one simulation, for check if setting is correct without submiting", action="store_true")
-    parser.add_argument('--testAndSubmit', '--ts', help ="Setup one simulation and submit job", action="store_true")
-    parser.add_argument('path', help='Path json to file containting simulation settings', default='flexpart_setup.py')
+#     parser.add_argument('--test', help="Setup one simulation, for check if setting is correct without submiting", action="store_true")
+#     parser.add_argument('--testAndSubmit', '--ts', help ="Setup one simulation and submit job", action="store_true")
+    parser.add_argument('path', help='Path json to file containting simulation settings')
     parser.add_argument('--absPath', '--ap', help='Absolute path to topdirectory where flexpart simulation will be created', default=None)
     parser.add_argument('--edate','--ed', help='date of last flexpart run', default=None)
     parser.add_argument('--bdate', '--bd', help='date of fist flexpart run', default=None)
@@ -217,10 +229,10 @@ if __name__=="__main__":
     test = False
     test_and_submit = False
 
-    if args.test:
-        test = True
-    elif args.testAndSubmit:
-        test_and_submit = True
+#     if args.test:
+#         test = True
+#     elif args.testAndSubmit:
+#         test_and_submit = True
     path = args.path
     abs_path = args.absPath
     e_date = args.edate
